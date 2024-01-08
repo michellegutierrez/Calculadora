91eeefd1d796f0254c2aa49b83dafe3bedb83946
michellegutierrez <micgutierrez17@gmail.com>
2024-01-08 14:30:29 -0500

    calculadora

--- calculadoraPromedio.js
+++ calculadoraPromedio.js
@@ -1,9 +1,9 @@
-/* //Desarrolla un programa que calcule el promedio de tres notas ingresadas 
+/* Desarrolla un programa que calcule el promedio de tres notas ingresadas 
 por el usuario, solicitando también el nombre del alumno. Además, el 
 programa debe verificar si las notas ingresadas se encuentran dentro del 
 rango válido (por ejemplo, de 0 a 10). Una vez calculado el promedio de las 
 tres notas, el programa debe evaluar si dicho promedio es mayor o igual a 
-7.*/
+7. */
 
 
 let suma = 0;
